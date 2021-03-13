bdaad65faf0a655be66d29a5de8209829c872be8
Kelwy <kelwyduarte@gmail.com>
2021-03-13 20:03:43 -0300

    Implements global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 const App: React.FC = () => (
   <>
-    <h1>Que o Challenge comece!</h1>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
   </>
 )
 
