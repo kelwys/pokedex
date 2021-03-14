ba6995ef1ba72339e452da3e03427e1842f055cc
Kelwy <kelwyduarte@gmail.com>
2021-03-14 19:17:57 -0300

    implements search and handles errors

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,55 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import { getAllPokemons } from '../../services/pokemons.service';
+import React, { FormEvent, useEffect, useState } from 'react';
+import { getAllPokemons, getPokemon } from '../../services/pokemons.service';
+
+import GridPokemons from '../../components/GridPokemons'
 
 import logoImg from '../../assets/logo.svg'
 
-import { Title, Form, Grid } from './styles';
+import { Title, Form, Error } from './styles';
 
 interface Pokemon {
   id: number;
   name: string;
 }
 
+
 const Home: React.FC = () => {
+  const [pokemon, setPokemon] = useState<Pokemon>();
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [newPokemon, setNewPokemon] = useState('');
+  const [inputError, setInputError] = useState('');
 
-
-  const loadAllPokemon = async () => {
+  const loadAllPokemons = async () => {
     const pokemons = await getAllPokemons();
     setPokemons(pokemons);
   }
 
   useEffect(() => {
-    loadAllPokemon();
+    loadAllPokemons();
   }, []);
+
+  async function handleSearchPokemon(
+    event: FormEvent<HTMLFormElement>,
+    ): Promise<void>{
+    event.preventDefault();
+
+    if (!newPokemon) {
+      setInputError('Digite o nome de um Pokemon!');
+      return;
+    }
+    try {
+      const pokemon: Pokemon = await getPokemon(newPokemon);
+      setPokemon(pokemon);
+      setNewPokemon('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse Pokemon!')
+    }
+
+  }
 
   return (
     <>
       <img src={logoImg} alt="Pokémon Explorer" />
       <Title>Explore pokémons na Pokédex</Title>
 
-      <Form>
-        <input placeholder="Digite o nome de um pokémon" />
+      <Form hasError={!!inputError} onSubmit={handleSearchPokemon}>
+        <input
+          value={newPokemon}
+          onChange={(e) => setNewPokemon(e.target.value)}
+          placeholder="Digite o nome de um pokémon"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
-      <Grid>
-        <div className="container">
-          {pokemons.map(pokemon => (
-            <div className="content" key={pokemon.name}>
-              <a href="#">
-                <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-                alt={pokemon.name}
-                />
-              </a>
-              <div>
-                <strong>{pokemon.name}</strong>
-              </div>
-            </div>
-          ))}
-        </div>
-      </Grid>
+      {inputError && <Error>{inputError}</Error>}
+
+      {pokemon ?
+        <h1>{pokemon.name}</h1>
+        :
+        <GridPokemons
+          pokemons={pokemons}
+        />
+      }
     </>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
 font-size: 48px;
@@ -10,7 +14,7 @@ line-height: 56px;
 margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
 margin-top: 40px;
 max-width: 700px;
 
@@ -23,6 +27,14 @@ input {
   border: 0;
   border-radius: 5px 0 0 5px;
   color: #3a3a3a;
+  border: 2px solid #fff;
+  border-right: 0;
+
+  ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
   &::placeholder {
     color: #a8a8b3;
@@ -45,35 +57,10 @@ button {
 }
 `;
 
-export const Grid = styled.div`
-  margin-top: 50px;
-  max-width: 700px;
-
-  .container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-  }
-
-  .content {
-    height: 140px;
-
-    background: #FFF;
-    border-radius: 5px;
-    margin: 5px;
-    padding: 5px;
-    display: block;
-    text-decoration: none;
-    text-align: center;
-
-    transition: transform 0.2s;
-
-    &:hover {
-      transform: translateX(2px)
-    }
-
-    strong {
-        color: #3d3d4d;
-      }
-  }
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+  font-size: 12px;
 `;
+
